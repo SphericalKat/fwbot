05928830f17a4dd6d313cf7634c81a68ed2d20e0
ATechnoHazard <amolele@firehound.org>
2018-07-18 12:18:21 +0530

    Fak aff forwardbot
    Signed-off-by: ATechnoHazard <amolele@firehound.org>

--- fwbot/__init__.py
+++ fwbot/__init__.py
@@ -12,15 +12,10 @@ ENV = bool(os.environ.get('ENV', False))
 
 if ENV:
 	TOKEN = os.environ.get('TOKEN', None)
-	FROM_CHAT = os.environ.get('FROM_CHAT', None)
-	TO_CHAT = os.environ.get('TO_CHAT', None)
 
 else:
 	from fwbot.Config import Vars
-
 	TOKEN = Vars.API_KEY
-	FROM_CHAT = Vars.FROM
-	TO_CHAT = Vars.FROM
 
 updater = tg.Updater(TOKEN, workers=16)
 
--- fwbot/__main__.py
+++ fwbot/__main__.py
@@ -1,20 +1,28 @@
-from fwbot import dispatcher, updater, TOKEN, LOGGER, FROM_CHAT, TO_CHAT
-from telegram import Message, Chat, Update, Bot, User
-from telegram.ext import MessageHandler, Filters
+from fwbot import dispatcher, updater, TOKEN, LOGGER
+from telegram import Message, Chat, Update, Bot, User, ParseMode
+from telegram.ext import CommandHandler, Filters
 from telegram.ext.dispatcher import run_async
+import requests, datetime, json, urllib
 
 @run_async
 def forward_message(bot: Bot, update: Update):
-	message = update.effective_message
-	try:
-		message.forward(int(TO_CHAT))
-	except BadRequest as excp:
-		LOGGER.info(excp.message)
+	send = "Devices to be built today are:\n"
+	dow = int(datetime.datetime.today().weekday() + 1)
+	devices = json.loads(urllib.request.urlopen("https://raw.githubusercontent.com/FireHound/jenkins/o8.1/build-targets.json").read().decode())
+	for device in devices:
+		if int(device.get("dow")) == dow:
+			send = send + "*" + str(device.get("device"))+ "*" + "\n"
 
+	update.effective_message.reply_text(send, parse_mode=ParseMode.MARKDOWN)
+
+def start(bot, update):
+	update.effective_message.reply_text("Yo, whadup")
 
 def main():
-	FORWARD_HANDLER = MessageHandler(Filters.chat(int(FROM_CHAT)), forward_message)
+	start_handler = CommandHandler ("start", start)
+	FORWARD_HANDLER = CommandHandler("devices", forward_message)
 	dispatcher.add_handler(FORWARD_HANDLER)
+	dispatcher.add_handler(start_handler)
 
 	LOGGER.info("Using long polling.")
 	updater.start_polling(timeout=15, read_latency=4)
